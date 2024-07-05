8ef5de735cced724ea67d349d25a876311f60bee
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-05 17:05:15 +0530

    Proxy middleware setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const morgan = require("morgan")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+
 
 const  { PORT } = require("./config/serverConfig")
 
 const app = express()
 
 app.use(morgan('combined'))
+
+app.use(
+    '/bookingservice',
+    createProxyMiddleware({
+      target: 'http://localhost:3002',
+      changeOrigin: true,
+    }),
+  );
 
 app.get("/home", (req, res) => {
     return res.status(200).json({

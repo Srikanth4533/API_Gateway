f9b28a626cbd51b329bedd35b7bf10dbe0c9c0e5
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-05 16:48:45 +0530

    Project setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const  { PORT } = require("./config/serverConfig")
 
 const app = express()
 
+app.get("/home", (req, res) => {
+    return res.status(200).json({
+        message: "Home Route"
+    })
+})
+
 const setupAndStart = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`)
